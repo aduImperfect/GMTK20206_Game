b13f8a6b744bd5feeebc69c06a19aeff0dc75100
Aditya Subramanian <adu101992@gmail.com>
2026-07-25 20:14:28 +0530

    hop jump like a bunny!

--- gmtk-game/Scripts/input_mgr.gd
+++ gmtk-game/Scripts/input_mgr.gd
@@ -196,8 +196,9 @@ func _physics_process(_delta: float) -> void:
 			action_in_progress = true
 		elif is_jumping_forward and not action_in_progress:
 			velocity.y = -(InputsData.max_jump_speed)
-			is_moving_forward = true
-			InputsData.move_speed = InputsData.max_move_speed
+			velocity.x = (InputsData.max_move_speed / 2)
+			#is_moving_forward = true
+			#InputsData.move_speed = InputsData.max_move_speed
 			action_in_progress = true
 		else:
 			grounded = true
